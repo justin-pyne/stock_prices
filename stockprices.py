45a4a2357a9b6ed91e5c9783f4c27b5da51465cf
justin-pyne <jpyne.justin@gmail.com>
2023-01-19 17:21:58 -0800

    stock prices done

--- stockprices.py
+++ stockprices.py
@@ -3,3 +3,49 @@ import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 
+dates = []
+prices = []
+
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader) #skipping column names
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[2]))
+            prices.append(float(row[1]))
+    return
+
+def predict_prices(dates, prices, x):
+    pass
+    dates = np.reshape(dates, (len(dates), 1))
+
+    svr_lin = SVR(kernel= 'linear', C=1e3)
+    svr_poly = SVR(kernel= 'poly', C=1e3, degree = 2)
+    svr_rbf = SVR(kernel= 'rbf', C=1e3, gamma=0.1)
+    svr_lin.fit(dates, prices)
+    svr_poly.fit(dates, prices)
+    svr_rbf.fit(dates, prices)
+
+    plt.scatter(dates, prices, color='black', label ='Data')
+    plt.plot(dates, svr_rbf.predict(dates), color= 'red', label='RBF model')
+    plt.plot(dates, svr_lin.predict(dates), color= 'green', label= 'Linear model')
+    plt.plot(dates,svr_poly.predict(dates), color= 'blue', label= 'Polynomial model')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Support Vector Regression')
+    plt.legend()
+    plt.show()
+
+    return svr_rbf.predict(np.array(x).reshape(-1,1))[0], svr_lin.predict(np.array(x).reshape(-1,1))[0], svr_poly.predict(np.array(x).reshape(-1,1))[0]
+
+
+get_data('AAPL.csv')
+print("Dates- ", dates)
+print("Prices- ", prices)
+
+predicted_price = predict_prices(dates, prices, 29)  
+print("\nThe stock open price for March 1st is predicted to be:")
+print("RBF kernel: $", str(predicted_price[0]))
+print("Linear kernel: $", str(predicted_price[1]))
+print("Polynomial kernel: $", str(predicted_price[2]))
+
